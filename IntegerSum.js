91a51b05d1e5ea743320f8d7a2f419cecaf80001
ijounjalkar <jounjalkarisha@gmail.com>
2020-11-13 17:00:56 +0530

    Added functionality to show sum of 3 integers adds to zero using Arrays

--- IntegerSum.js
+++ IntegerSum.js
@@ -1,7 +1,10 @@
-var array = new Array();
-for(var i = 1 ; i < 100 ; i++){
-    if(i % 11 == 0){
-        array.push(i);
+var array = new Array(-1, 0, 1, 2, -1, -4);
+array.forEach(i=>console.log(i));
+for (var i = 0; i < array.length - 2; i++) {
+    for (var j = i + 1; j < array.length - 1; j++) {
+        for (var k = j + 1; k < array.length; k++) {
+            if (array[i] + array[j] + array[k] == 0) {
+                console.log("Sum of elements : " + array[i] + " " + array[j] + " " + array[k] + " " + "is zero");            }
+        }
     }
-}
-array.forEach(i => console.log(i));
+} 
